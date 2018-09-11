f796dfd8b2e04d04e0b86637ffe42e1885faa8d9
Vladimir Corja <Vladimir.Corja@asurion.com>
2018-09-11 14:11:06 -0400

    Added JsHint checker and Mocha test structure

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 // Require all dependencies
 const express = require('express');
 const app = express();
+const compression = require('compression');
+const helmet = require('helmet');
+
+// Compress all responses
+app.use(compression());
+
+// Use Helmet to protect from some well-known web vulnerabilities by setting HTTP headers appropriately
+app.use(helmet());
 
 // Connect all routes for the application
 app.get('/', function(req, res) {
